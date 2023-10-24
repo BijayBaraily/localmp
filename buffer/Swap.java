a0916720e762a57b2f79400efae42c975024e1f2
BijayBaraily <aaungpwal@gmail.com>
2023-10-24 21:44:22 +0545

    updated swap file

---
+++ buffer/Swap.java
@@ -0,0 +1,13 @@
+class Swap{
+    public static void main(String[] args) {
+        int a=5;
+        int b=7;
+        System.out.println(a);
+        System.out.println(b);
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
